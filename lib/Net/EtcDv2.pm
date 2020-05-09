a05dab824424536213c642b953b0092459a37851
Gary Greene <greeneg@tolharadys.net>
2020-05-09 13:35:33 -0400

    Initial check-in of generated code
    Signed-off-by: Gary Greene <greeneg@tolharadys.net>

---
+++ lib/Net/EtcDv2.pm
@@ -0,0 +1,109 @@
+package Net::EtcDv2;
+
+use 5.006;
+use strict;
+use warnings;
+
+=head1 NAME
+
+Net::EtcDv2 - The great new Net::EtcDv2!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Net::EtcDv2;
+
+    my $foo = Net::EtcDv2->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Gary L. Greene, Jr., C<< <greeneg at tolharadys.net> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-net-etcdv2 at rt.cpan.org>, or through
+the web interface at L<https://rt.cpan.org/NoAuth/ReportBug.html?Queue=Net-EtcDv2>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Net::EtcDv2
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<https://rt.cpan.org/NoAuth/Bugs.html?Dist=Net-EtcDv2>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Net-EtcDv2>
+
+=item * CPAN Ratings
+
+L<https://cpanratings.perl.org/d/Net-EtcDv2>
+
+=item * Search CPAN
+
+L<https://metacpan.org/release/Net-EtcDv2>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+This software is Copyright (c) 2020 by Gary L. Greene, Jr.
+
+This is free software, licensed under:
+
+  The Apache License, Version 2.0, January 2004
+
+
+=cut
+
+1; # End of Net::EtcDv2
---
+++ t/00-load.t
@@ -0,0 +1,13 @@
+#!perl -T
+use 5.006;
+use strict;
+use warnings;
+use Test::More;
+
+plan tests => 1;
+
+BEGIN {
+    use_ok( 'Net::EtcDv2' ) || print "Bail out!\n";
+}
+
+diag( "Testing Net::EtcDv2 $Net::EtcDv2::VERSION, Perl $], $^X" );
